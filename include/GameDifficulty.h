209d058bdf20b99bdaa7156a4eddfd7f12fd8bb7
LiaoAnn <1050123tom@gmail.com>
2023-06-11 16:56:05 +0800

    feat: added game-diffculty and game-mode

---
+++ include/GameDifficulty.h
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * File: GameDifficulty.h
+ * Author: ¹ù§Ê¦w (B11130237)
+ * Create Date: 2023-06-11
+ * Editor: ¹ù§Ê¦w (B11130237)
+ * Update Date: 2023-06-11
+ * Description: Header file of GameDifficulty enum
+ ***********************************************************************/
+
+#pragma once
+
+enum GameDifficulty {
+	EASY,
+	NORMAL,
+	HARD
+};
---
+++ include/GameMode.h
@@ -0,0 +1,17 @@
+/***********************************************************************
+ * File: GameMode.h
+ * Author: ¹ù§Ê¦w (B11130237)
+ * Create Date: 2023-06-11
+ * Editor: ¹ù§Ê¦w (B11130237)
+ * Update Date: 2023-06-11
+ * Description: Header file of GameMode enum
+ ***********************************************************************/
+
+#pragma once
+
+enum GameMode
+{
+	NEW_GAME,
+	LOAD_GAME,
+	EXIT_GAME
+};
